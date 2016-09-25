e69433e70388b9782980b3752858a4169e6cc3af
vagrant <vagrant@homestead>
2016-09-25 20:18:48 +0000

    route group product

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,6 @@ Route::get('/', function () {
 
 Route::group(['prefix' => 'admin', 'as' => 'admin.', 'namespace' => 'Backend'], function() {
 	Route::resource('category', 'CategoryController');
-});
-
-Route::group(['prefix' => 'admin', 'as' => 'admin.', 'namespace' => 'Backend'], function() {
 	Route::resource('product', 'ProductController');
 });
+
